d0f84c669eaa99033a2dfb76067a3d7cea0018b8
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-11 18:29:25 +0530

    add theme header and footer

---
+++ wp-content/themes/basic-theme/header.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Header template.
+ * 
+ * @package Basic_theme
+ */
+
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset= "UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
