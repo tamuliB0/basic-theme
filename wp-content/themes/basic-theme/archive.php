8cebe4cdf64491a8df888bc10e9025dabb2a150b
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-13 16:36:56 +0530

    add archive template for archive listings

---
+++ wp-content/themes/basic-theme/archive.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template for archive page.
+ *
+ * @package Basic_Theme
+ */
+
+get_header();?>
+<h1><?php the_archive_title(); ?></h1>
+<?php
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		?>
+		<?php get_template_part( 'template-parts/content' ); ?>
+		<?php
+	endwhile;
+else :
+	esc_html_e( 'Sorry, no posts matched your criteria.', '' );
+endif;
+
+get_footer();
+
