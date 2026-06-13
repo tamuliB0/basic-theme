a7a734e745c59e55b4c9b445ee3f43cb2e782d8e
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-13 10:58:06 +0530

    add theme support for post thumbnails and title-tag

--- wp-content/themes/basic-theme/functions.php
+++ wp-content/themes/basic-theme/functions.php
@@ -17,3 +17,12 @@ function basic_theme_enqueue_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'basic_theme_enqueue_styles' );
+
+/**
+ * Sets up theme features.
+ */
+function basic_theme_setup() {
+	add_theme_support( 'post-thumbnails', array( 'post' ) );
+	add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'basic_theme_setup' );
