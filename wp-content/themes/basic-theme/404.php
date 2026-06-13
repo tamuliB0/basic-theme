ea7cdad7b27d2a3283bc98369eca812ba6a9c430
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-13 12:32:44 +0530

    add page template for 404 error status

---
+++ wp-content/themes/basic-theme/404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template for 404 pages (page not found)
+ *
+ * @package Basic_theme
+ */
+
+get_header(); ?>
+
+<h1>404 - Page not found</h1>
+<p>The page that you're looking for doesn't exist </p>
+
+<?php get_footer(); ?>
