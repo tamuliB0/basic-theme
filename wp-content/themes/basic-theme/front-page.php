b3d353e3e2669a8b2a17c9919d5227df5834fc8c
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-13 11:02:43 +0530

    fix whitespace in homepage template

--- wp-content/themes/basic-theme/front-page.php
+++ wp-content/themes/basic-theme/front-page.php
@@ -8,12 +8,8 @@
 get_header();
 ?>
 
-<h1> 
-    <?php bloginfo( 'name' ); ?>
-</h1>
+<h1><?php bloginfo( 'name' ); ?></h1>
 
-<p>
-    <?php esc_html_e( 'Welcome to my WordPress site where I share blog posts and portfolio work.', 'basic-theme' ); ?>
-</p>
+<p><?php esc_html_e( 'Welcome to my WordPress site where I share blog posts and portfolio work.', 'basic-theme' ); ?></p>
 
 <?php get_footer(); ?>
