e264f0f5b1f88bffc1dec82b6118c2acd1c34ac5
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-15 17:20:47 +0530

    Update portfolio single template layout

--- wp-content/themes/basic-theme/single-portfolio.php
+++ wp-content/themes/basic-theme/single-portfolio.php
@@ -10,10 +10,17 @@ get_header();
 if ( have_posts() ) :
     while ( have_posts() ) :
         the_post();
-        get_template_part( 'template-parts/content' );
+        ?>
+        <article <?php post_class(); ?>>
+            <?php if ( has_post_thumbnail() ) : ?>
+                <?php the_post_thumbnail( 'large' ); ?>
+            <?php endif; ?>
+            <h1><?php echo esc_html( get_the_title() ); ?></h1>
+            <?php the_content(); ?>
+        </article>
+    <?php
     endwhile;
 else :
     esc_html_e( 'No portfolio item found.', 'basic-theme' );
 endif;
-
 get_footer();
