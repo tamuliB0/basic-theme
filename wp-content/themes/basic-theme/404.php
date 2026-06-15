db9d6b63e5f1c55727adc15356fc1014aab71a22
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2026-06-15 16:57:08 +0530

    update 404.php to escape and translate text

--- wp-content/themes/basic-theme/404.php
+++ wp-content/themes/basic-theme/404.php
@@ -5,9 +5,10 @@
  * @package Basic_theme
  */
 
-get_header(); ?>
+get_header(); 
+?>
 
-<h1>404 - Page not found</h1>
-<p>The page that you're looking for doesn't exist </p>
+<h1><?php esc_html_e( '404 - Page not found', 'basic-theme' ); ?></h1>
+<p><?php esc_html_e( 'The page that you are looking for does not exist.', 'basic-theme' ); ?></p>
 
 <?php get_footer(); ?>
